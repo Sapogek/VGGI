d4299de42e379d76933def4f855b121b6387def6
Sapogek <mrrexik1096@gmail.com>
2025-12-11 01:44:10 +0200

    ЛР №3 Normal mapping з пріоритетом Тангенсу та обчисленням TBN

--- SurfaceModel.js
+++ SurfaceModel.js
@@ -5,9 +5,13 @@ function SurfaceModel(gl, shProgram) {
     this.shProgram = shProgram;
     this.iVertexBuffer = gl.createBuffer();
     this.iNormalBuffer = gl.createBuffer();
-    this.iIndexBuffer = gl.createBuffer();
-    this.nFaces = 0;    
+    this.iTexCoordBuffer = gl.createBuffer();
 
+    this.iTangentBuffer = gl.createBuffer();
+    this.iBitangentBuffer = gl.createBuffer();
+    this.iIndexBuffer = gl.createBuffer();
+    
+    this.nFaces = 0;    
 
     this.p = 0.1;
     this.m = 0.1;
@@ -18,17 +22,14 @@ function SurfaceModel(gl, shProgram) {
     this.vMin = 0.0;
     this.vMax = 2.0 * Math.PI;
  
-    this.setParameters = function(newP, newM, newUMax, newVMaxFactor, newNumSegments) {
+    this.setParameters = function(newP, newM, newUMax, newVMaxFactor) {
         this.p = newP;
         this.m = newM; 
         this.uMax = newUMax;
-        this.numU = newNumSegments;
-        this.numV = newNumSegments;
         this.vMax = newVMaxFactor * Math.PI;
     }
 
     this.SurfacePoint = function(u, v) {
-
         let expP_U = Math.exp(this.p * u); 
         let expM_U = Math.exp(this.m * u);
         let factor = expM_U + expP_U * Math.cos(v);
@@ -40,112 +41,158 @@ function SurfaceModel(gl, shProgram) {
         return [x, y, z];
     }  
     
+    //  Часткова похідна за u
+    this.SurfaceDerivativeU = function(u, v) {
+        let expP_U = Math.exp(this.p * u);
+        let expM_U = Math.exp(this.m * u);
+        let cosU = Math.cos(u);
+        let sinU = Math.sin(u);
+        let cosV = Math.cos(v);
+        let sinV = Math.sin(v);
+        
+        let factor = expM_U + expP_U * cosV;
+        let dFactor_dU = this.m * expM_U + this.p * expP_U * cosV;
+        
+        let dx = dFactor_dU * cosU - factor * sinU;
+        let dy = dFactor_dU * sinU + factor * cosU;
+        let dz = this.p * expP_U * sinV;
+        
+        return [dx, dy, dz];
+    }
     
+    // Часткова похідна за v
+    this.SurfaceDerivativeV = function(u, v) {
+        let expP_U = Math.exp(this.p * u);
+        let cosU = Math.cos(u);
+        let sinU = Math.sin(u);
+        let cosV = Math.cos(v);
+        let sinV = Math.sin(v);
+        
+        let dFactor_dV = -expP_U * sinV;
+        
+        let dx = dFactor_dV * cosU;
+        let dy = dFactor_dV * sinU;
+        let dz = expP_U * cosV;
+        
+        return [dx, dy, dz];
+    }
 
     this.GenerateSurfaceAndNormals = function() {
-    let vertices = [];
-    let normals = [];
-    let indices = [];
-    
-    let uStep = (this.uMax - this.uMin) / this.numU;
-    let vStep = (this.vMax - this.vMin) / this.numV;
-    
-    for (let i = 0; i <= this.numU; i++) {
-        let u = this.uMin + i * uStep;
-        for (let j = 0; j <= this.numV; j++) {
-            let v = this.vMin + j * vStep;
-            
-            let point = this.SurfacePoint(u, v);
-            vertices.push(...point);
-            
-            normals.push(0, 0, 0); 
-        }
-    }
-
-    let rows = this.numU + 1;
-    let cols = this.numV + 1;
-    for (let i = 0; i < this.numU; i++) {
-        for (let j = 0; j < this.numV; j++) {
-            let i1 = i * cols + j;
-            let i2 = i1 + cols;
-            let i3 = i1 + 1;
-            let i4 = i2 + 1;
-            
-            // Трикутник 1: (i1, i2, i3)
-            indices.push(i1, i2, i3); 
-            // Трикутник 2: (i3, i2, i4)
-            indices.push(i3, i2, i4);
-        }
-    }
-    this.nFaces = indices.length;
-
-
-    const computeFaceNormal = (i1, i2, i3) => {
-        // Отримання координат вершин V1, V2, V3
-        let V1 = vertices.slice(i1 * 3, i1 * 3 + 3);
-        let V2 = vertices.slice(i2 * 3, i2 * 3 + 3);
-        let V3 = vertices.slice(i3 * 3, i3 * 3 + 3);
-
-        // Обчислення векторів 
-        let E12 = [V2[0]-V1[0], V2[1]-V1[1], V2[2]-V1[2]];
-        let E13 = [V3[0]-V1[0], V3[1]-V1[1], V3[2]-V1[2]];
+        let vertices = [];
+        let normals = [];
+        let texCoords = [];
+        let tangents = [];
+        let bitangents = [];
+        let indices = [];
         
-        // Нормаль грані
-        let N = m4.cross(E12, E13); 
-        m4.normalize(N, N);
-        return N;
+        let uStep = (this.uMax - this.uMin) / this.numU;
+        let vStep = (this.vMax - this.vMin) / this.numV;
+        
+        for (let i = 0; i <= this.numU; i++) {
+            let u = this.uMin + i * uStep;
+            for (let j = 0; j <= this.numV; j++) {
+                let v = this.vMin + j * vStep;
+                
+                // Позиція вершини
+                let point = this.SurfacePoint(u, v);
+                vertices.push(...point);
+                
+                // Текстурні координати (нормалізовані параметри u,v)
+                let s = i / this.numU;
+                let t = j / this.numV;
+                texCoords.push(s, t);
+                
+                // Обчислення часткових похідних (тангенти і бітангенти в об'єктному просторі)
+                let dPdu = this.SurfaceDerivativeU(u, v);
+                let dPdv = this.SurfaceDerivativeV(u, v);
+                
+                // Нормаль поверхні
+                let normal = m4.cross(dPdu, dPdv);
+                m4.normalize(normal, normal);
+                normals.push(...normal);
+                
+                // Variant 23: Prioritize tangent в Гранд-Шмітт нормалізації
+                // T' = T 
+                let T = [...dPdu];
+                m4.normalize(T, T);
+                
+                // B' = N × T'
+                let B = m4.cross(normal, T);
+                m4.normalize(B, B);
+                
+                // N' = T' × B' 
+                let N = m4.cross(T, B);
+                m4.normalize(N, N);
+                
+                tangents.push(...T);
+                bitangents.push(...B);
+                
+                // Оновлення нормалі
+                normals[normals.length - 3] = N[0];
+                normals[normals.length - 2] = N[1];
+                normals[normals.length - 1] = N[2];
+            }
+        }
+
+        let cols = this.numV + 1;
+        for (let i = 0; i < this.numU; i++) {
+            for (let j = 0; j < this.numV; j++) {
+                let i1 = i * cols + j;
+                let i2 = i1 + cols;
+                let i3 = i1 + 1;
+                let i4 = i2 + 1;
+                
+                indices.push(i1, i2, i3); 
+                indices.push(i3, i2, i4);
+            }
+        }
+        this.nFaces = indices.length;
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iTexCoordBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iTangentBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tangents), gl.STATIC_DRAW);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iBitangentBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(bitangents), gl.STATIC_DRAW);
+        
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
     };
 
-    // Обнуляємо масив нормалей для усереднення
-    let averagedNormals = new Array(vertices.length).fill(0);
-
-    for (let k = 0; k < indices.length; k += 3) {
-        let i1 = indices[k], i2 = indices[k+1], i3 = indices[k+2];
-        let faceNormal = computeFaceNormal(i1, i2, i3);
-        
-        // Сумуємо нормаль грані до нормалей вершин, що її утворюють
-        for (let idx of [i1, i2, i3]) {
-            averagedNormals[idx * 3]   += faceNormal[0];
-            averagedNormals[idx * 3 + 1] += faceNormal[1];
-            averagedNormals[idx * 3 + 2] += faceNormal[2];
-        }
-    }
-    
-    // Нормалізуємо усереднені нормалі
-    for (let i = 0; i < averagedNormals.length; i += 3) {
-        let N = averagedNormals.slice(i, i + 3);
-        m4.normalize(N, N);
-        normals[i] = N[0];
-        normals[i+1] = N[1];
-        normals[i+2] = N[2];
-    }
-    
-    // 4. Завантаження даних у GPU
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
-    
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-};
-
     this.initBuffers = function() {
-    this.GenerateSurfaceAndNormals();
-};
+        this.GenerateSurfaceAndNormals();
+    };
     
     this.Draw = function() {
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-    gl.vertexAttribPointer(this.shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(this.shProgram.iAttribVertex);
-    
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
-    gl.vertexAttribPointer(this.shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(this.shProgram.iAttribNormal);
-    
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
-    gl.drawElements(gl.TRIANGLES, this.nFaces, gl.UNSIGNED_SHORT, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.vertexAttribPointer(this.shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.shProgram.iAttribVertex);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+        gl.vertexAttribPointer(this.shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.shProgram.iAttribNormal);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iTexCoordBuffer);
+        gl.vertexAttribPointer(this.shProgram.iAttribTexCoord, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.shProgram.iAttribTexCoord);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iTangentBuffer);
+        gl.vertexAttribPointer(this.shProgram.iAttribTangent, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.shProgram.iAttribTangent);
+        
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iBitangentBuffer);
+        gl.vertexAttribPointer(this.shProgram.iAttribBitangent, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(this.shProgram.iAttribBitangent);
+        
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+        gl.drawElements(gl.TRIANGLES, this.nFaces, gl.UNSIGNED_SHORT, 0);
     }
-}  
+}
--- main.js
+++ main.js
@@ -1,205 +1,177 @@
 'use strict';
 
-let gl;                         // The webgl context.
-let surface;                    // A surface model
-let shProgram;                  // A shader program
-let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
+let gl;
+let surface;
+let shProgram;
+let spaceball;
 let updateTimeout;
-/**
- (перетворення точки)
- @param {Array<number>} m 
- @param {Array<number>} v 
- @returns {Array<number>} 
- */
+
+// Текстурні об'єкти
+let diffuseTexture;
+let specularTexture;
+let normalTexture;
+
 function m4MultiplyVector(m, v) {
     let result = new Array(4);
-    
-    result[0] = m[0]*v[0] + m[4]*v[1] + m[8]*v[2]  + m[12]*v[3]; // X
-    result[1] = m[1]*v[0] + m[5]*v[1] + m[9]*v[2]  + m[13]*v[3]; // Y
-    result[2] = m[2]*v[0] + m[6]*v[1] + m[10]*v[2] + m[14]*v[3]; // Z
-    result[3] = m[3]*v[0] + m[7]*v[1] + m[11]*v[2] + m[15]*v[3]; // W
-    
+    result[0] = m[0]*v[0] + m[4]*v[1] + m[8]*v[2]  + m[12]*v[3];
+    result[1] = m[1]*v[0] + m[5]*v[1] + m[9]*v[2]  + m[13]*v[3];
+    result[2] = m[2]*v[0] + m[6]*v[1] + m[10]*v[2] + m[14]*v[3];
+    result[3] = m[3]*v[0] + m[7]*v[1] + m[11]*v[2] + m[15]*v[3];
     return result;
 }
+
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
 
-
-// Constructor
-function Model(name) {
-    this.name = name;
-    this.iVertexBuffer = gl.createBuffer();
-    this.count = 0;
-
-    this.BufferData = function(vertices) {
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
-
-        this.count = vertices.length/3;
-    }
-
-    this.Draw = function() {
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(shProgram.iAttribVertex);
-   
-        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-    }
-}
-
-
-// Constructor
 function ShaderProgram(name, program) {
-
     this.name = name;
     this.prog = program;
-
-    // Location of the attribute variable in the shader program.
     this.iAttribVertex = -1;
-    // Location of the uniform specifying a color for the primitive.
-    this.iColor = -1;
-    // Location of the uniform matrix representing the combined transformation.
-    this.iModelViewProjectionMatrix = -1;
+    this.iAttribNormal = -1;
+    this.iAttribTexCoord = -1;
+    this.iAttribTangent = -1;
+    this.iAttribBitangent = -1;
+    
+    this.iModelViewMatrix = -1;
+    this.iProjectionMatrix = -1;
+    this.iNormalMatrix = -1;
+    this.iLightPosition = -1;
+    this.iAmbientColor = -1;
+    this.iDiffuseColor = -1;
+    this.iSpecularColor = -1;
+    this.iShininess = -1;
+    
+    this.iDiffuseTexture = -1;
+    this.iSpecularTexture = -1;
+    this.iNormalTexture = -1;
 
     this.Use = function() {
         gl.useProgram(this.prog);
     }
 }
 
-
-/* Draws a colored cube, along with a set of coordinate axes.
- * (Note that the use of the above drawPrimitive function is not an efficient
- * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
- */
 function draw() { 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
-    // 1. Обчислення Матриць Перетворення 
-    
-    // 1a. View Matrix (від Trackball Rotator)
     let viewMatrix = spaceball.getViewMatrix(); 
-    console.log("View Matrix:", viewMatrix);
- 
-    // 1b. Model Matrix 
     let translateToPointZero = m4.translation(0, 0, 0);
-
     let modelMatrix = translateToPointZero; 
- 
-    // 1c. ModelView Matrix (View * Model)
     let modelViewMatrix = m4.multiply(viewMatrix, modelMatrix);
-    
-    // 1d. Projection Matrix 
     let projectionMatrix = m4.perspective(Math.PI / 8, 1, 0.01, 1000); 
-
-    // 1e. Normal Matrix обернена транспонована ModelView
     let normalMatrix = m4.transpose(m4.inverse(modelViewMatrix));
-    
-    // 2. Розрахунок Позиції Світла
     
     let time = performance.now() * 0.0005; 
     let lightRadius = 150.0;
     let lightHeight = 50.0; 
     let lightX = lightRadius * Math.cos(time); 
     let lightZ = lightRadius * Math.sin(time);
-
-    // 2a. Позиція світла 
     let lightPositionWorld = [lightX, lightHeight, lightZ, 1.0]; 
-    
-    // 2b. Позиція світла (View Space = ViewMatrix * WorldPosition)
     let lightPositionView = m4MultiplyVector(viewMatrix, lightPositionWorld);
     
-    // --- 3. Передача Уніформ до GPU ---
-    
-    // Матриці
     gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, modelViewMatrix);
     gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projectionMatrix);
     gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
-
-    // Параметри освітлення 
     gl.uniform4fv(shProgram.iLightPosition, lightPositionView);
-    gl.uniform4f(shProgram.iAmbientColor, 0.1, 0.1, 0.1, 1.0); 
-    gl.uniform4f(shProgram.iDiffuseColor, 0.0, 0.5, 0.8, 1.0); 
-    gl.uniform4f(shProgram.iSpecularColor, 1.0, 1.0, 1.0, 1.0);
+    gl.uniform4f(shProgram.iAmbientColor, 0.5, 0.5, 0.5, 1.0);
+    gl.uniform4f(shProgram.iDiffuseColor, 1.5, 1.5, 1.5, 1.0);
+    gl.uniform4f(shProgram.iSpecularColor, 1.8, 1.8, 1.8, 1.0); 
     gl.uniform1f(shProgram.iShininess, 50.0); 
+    
+    // Зв'язування текстур
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, diffuseTexture);
+    gl.uniform1i(shProgram.iDiffuseTexture, 0);
+    
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, specularTexture);
+    gl.uniform1i(shProgram.iSpecularTexture, 1);
+    
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, normalTexture);
+    gl.uniform1i(shProgram.iNormalTexture, 2);
     
     surface.Draw();
     
     window.requestAnimationFrame(draw); 
 }
 
-function CreateSurfaceData()
-{
-    let vertexList = [];
-
-    for (let i=0; i<360; i+=5) {
-        vertexList.push( Math.sin(deg2rad(i)), 1, Math.cos(deg2rad(i)) );
-        vertexList.push( Math.sin(deg2rad(i)), 0, Math.cos(deg2rad(i)) );
-    }
-
-    return vertexList;
+function loadTexture(url, callback) {
+    const texture = gl.createTexture();
+    const image = new Image();
+    
+    image.onload = function() {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+        
+        if (callback) callback(texture);
+    };
+    
+    image.src = url;
+    return texture;
 }
 
-
-/* Initialize the WebGL context. Called from init() */
 function initGL() {
-    let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
+    let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
-    shProgram = new ShaderProgram('PhongShading', prog);
+    shProgram = new ShaderProgram('TexturedPhongShading', prog);
     shProgram.Use();
 
-    shProgram.iAttribVertex     = gl.getAttribLocation(prog, "vertex");
-    shProgram.iAttribNormal     = gl.getAttribLocation(prog, "normal"); 
+    shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal = gl.getAttribLocation(prog, "normal"); 
+    shProgram.iAttribTexCoord = gl.getAttribLocation(prog, "texCoord");
+    shProgram.iAttribTangent = gl.getAttribLocation(prog, "tangent");
+    shProgram.iAttribBitangent = gl.getAttribLocation(prog, "bitangent");
 
-    // Матриці
-    shProgram.iModelViewMatrix  = gl.getUniformLocation(prog, "ModelViewMatrix");
+    shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
     shProgram.iProjectionMatrix = gl.getUniformLocation(prog, "ProjectionMatrix");
-    shProgram.iNormalMatrix     = gl.getUniformLocation(prog, "NormalMatrix");
-
-    // Освітлення та матеріал
-    shProgram.iLightPosition    = gl.getUniformLocation(prog, "LightPosition");
-    shProgram.iAmbientColor     = gl.getUniformLocation(prog, "AmbientColor");
-    shProgram.iDiffuseColor     = gl.getUniformLocation(prog, "DiffuseColor");
-    shProgram.iSpecularColor    = gl.getUniformLocation(prog, "SpecularColor");
-    shProgram.iShininess        = gl.getUniformLocation(prog, "Shininess");
+    shProgram.iNormalMatrix = gl.getUniformLocation(prog, "NormalMatrix");
+    shProgram.iLightPosition = gl.getUniformLocation(prog, "LightPosition");
+    shProgram.iAmbientColor = gl.getUniformLocation(prog, "AmbientColor");
+    shProgram.iDiffuseColor = gl.getUniformLocation(prog, "DiffuseColor");
+    shProgram.iSpecularColor = gl.getUniformLocation(prog, "SpecularColor");
+    shProgram.iShininess = gl.getUniformLocation(prog, "Shininess");
+    
+    shProgram.iDiffuseTexture = gl.getUniformLocation(prog, "diffuseTexture");
+    shProgram.iSpecularTexture = gl.getUniformLocation(prog, "specularTexture");
+    shProgram.iNormalTexture = gl.getUniformLocation(prog, "normalTexture");
     
     surface = new SurfaceModel(gl, shProgram);
     surface.initBuffers();
 
+    // Завнтаження текстур
+    diffuseTexture = loadTexture('texturess/diffuse.jpg');
+    specularTexture = loadTexture('texturess/specular.jpg');
+    normalTexture = loadTexture('texturess/normal.jpg');
+
     gl.enable(gl.DEPTH_TEST); 
 }
 
-
-/* Creates a program for use in the WebGL context gl, and returns the
- * identifier for that program.  If an error occurs while compiling or
- * linking the program, an exception of type Error is thrown.  The error
- * string contains the compilation or linking error.  If no error occurs,
- * the program identifier is the return value of the function.
- * The second and third parameters are strings that contain the
- * source code for the vertex shader and for the fragment shader.
- */
 function createProgram(gl, vShader, fShader) {
-    let vsh = gl.createShader( gl.VERTEX_SHADER );
-    gl.shaderSource(vsh,vShader);
+    let vsh = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vsh, vShader);
     gl.compileShader(vsh);
-    if ( ! gl.getShaderParameter(vsh, gl.COMPILE_STATUS) ) {
-        throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
-     }
-    let fsh = gl.createShader( gl.FRAGMENT_SHADER );
+    if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in vertex shader: " + gl.getShaderInfoLog(vsh));
+    }
+    let fsh = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fsh, fShader);
     gl.compileShader(fsh);
-    if ( ! gl.getShaderParameter(fsh, gl.COMPILE_STATUS) ) {
-       throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
+        throw new Error("Error in fragment shader: " + gl.getShaderInfoLog(fsh));
     }
     let prog = gl.createProgram();
-    gl.attachShader(prog,vsh);
+    gl.attachShader(prog, vsh);
     gl.attachShader(prog, fsh);
     gl.linkProgram(prog);
-    if ( ! gl.getProgramParameter( prog, gl.LINK_STATUS) ) {
-       throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+        throw new Error("Link error in program: " + gl.getProgramInfoLog(prog));
     }
     return prog;
 }
@@ -215,54 +187,37 @@ function updateGranularity(axis) {
         } else if (axis === 'V') {
             surface.numV = value;
         }
-        
         surface.initBuffers(); 
-        draw(); 
     }
 }
 
 function updateSurface() {
     clearTimeout(updateTimeout);
-
     updateTimeout = setTimeout(() => {
-        // Коефіцієнт p
         let p = parseFloat(document.getElementById("pInput").value);
         document.getElementById("pValue").textContent = p.toFixed(2); 
-        
-        // Коефіцієнт m
         let m = parseFloat(document.getElementById("mInput").value);
         document.getElementById("mValue").textContent = m.toFixed(2);
-
-        // U Max
         let uMax = parseFloat(document.getElementById("uMaxInput").value);
         document.getElementById("uMaxValue").textContent = uMax.toFixed(2);
-        
-        // V Max
         let vMaxFactor = parseFloat(document.getElementById("vMaxInput").value);
         document.getElementById("vMaxValue").textContent = vMaxFactor.toFixed(1);
         
         if (surface && !isNaN(p) && !isNaN(m) && !isNaN(uMax) && !isNaN(vMaxFactor)) {
-            
-
             surface.setParameters(p, m, uMax, vMaxFactor);
-
             surface.numU = parseInt(document.getElementById("numUSlider").value);
             surface.numV = parseInt(document.getElementById("numVSlider").value);
-            
             surface.initBuffers();
-            draw();
         } 
     }, 100); 
 }
-/**
- * initialization function that will be called when the page has loaded
- */
+
 function init() {
     let canvas;
     try {
         canvas = document.getElementById("webglcanvas");
         gl = canvas.getContext("webgl");
-        if ( ! gl ) {
+        if (!gl) {
             throw "Browser does not support WebGL";
         }
     }
@@ -272,7 +227,7 @@ function init() {
         return;
     }
     try {
-        initGL();  // initialize the WebGL graphics context
+        initGL();
     }
     catch (e) {
         document.getElementById("canvas-holder").innerHTML =
@@ -281,6 +236,5 @@ function init() {
     }
 
     spaceball = new TrackballRotator(canvas, null, 100);
-
     window.requestAnimationFrame(draw);
 }
